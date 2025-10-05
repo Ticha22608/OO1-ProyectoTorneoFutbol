4ab44ccabd6451d9e35ca8624b90b0e4ccd9c9b3
Ticha <ticha22608@gmail.com>
2025-10-05 18:56:03 -0300

    Revert "Modificaciones respecto a casos de Uso"

--- src/modelo/Entrenador.java
+++ src/modelo/Entrenador.java
@@ -10,7 +10,7 @@ public class Entrenador extends Persona{
     	super(idPersona, nombre, apellido, dni, fechaNacimiento);
         this.estrategiaFavorita = estrategiaFavorita;
     }
-
+    
 	public String getEstrategiaFavorita() {
         return estrategiaFavorita;
     }
@@ -18,8 +18,7 @@ public class Entrenador extends Persona{
     public void setEstrategiaFavorita(String estrategiaFavorita) {
         this.estrategiaFavorita = estrategiaFavorita;
     }
-
-
+    
 
 	@Override
 	public String toString() {
@@ -37,5 +36,5 @@ public class Entrenador extends Persona{
 		Entrenador other = (Entrenador) obj;
 		return Objects.equals(estrategiaFavorita, other.estrategiaFavorita);
 	}
-
+    
 }
